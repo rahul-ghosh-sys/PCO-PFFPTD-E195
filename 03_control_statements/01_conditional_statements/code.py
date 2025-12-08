b75bf48f0bf36362601339abf687539f45828f72
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-12-08 20:59:37 +0530

    Adding questions for if statement along with notes

--- 03_control_statements/01_conditional_statements/code.py
+++ 03_control_statements/01_conditional_statements/code.py
@@ -1,5 +1,30 @@
-# WAP to ask the user for a number. Check if the number is positive or not. If positive, then print "It is a positive number".
+# WAP(Write a program) to ask the user for a number. Check if the number is positive or not. If positive, then print "It is a positive number".
+'''
+Algorithm:
+-- We should take a number from the user.
+-- Now we have to check whether the number is positive or not.
+-- If the number is positive, print "It is a positive number."
+-- If the number is not positive, do nothing.
+'''
+# "input()" is an in-built function, which is used to take input from the user. That input will be in the form of "str".
+num = int(input('Enter a number: '))
+# "if" block will get executed only the condition will be True.
+if num > 0:
+    # It is known as TSB(True Statement Block).
+    print('It is a positive number !')
+
+
 
 #  WAP to ask the user to put a temperature value. If the temperature is greater than 30 then print "Too much hot weather!"
+'''
+Algorithm:
+-- We have to take one temperature value from the user.
+-- Check temperature is greater than 30 or not.
+-- If greater than 30, print "Too much hot weather!"
+-- If the temperature is not greater than 30, do nothing.
+'''
+temp = int(input('Enter temperature: '))
+if temp > 30:
+    print('Too much hot weather!')
 
 # WAP to take one number from the user and check it is divisible by 5 or not. If divisible by 5 then print "True".

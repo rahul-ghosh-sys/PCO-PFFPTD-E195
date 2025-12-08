f1ee36a8f75f68a82bc93b72fb1be787d6ed956a
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-12-08 19:48:47 +0530

    Adding code file for control statements

---
+++ 03_control_statements/01_conditional_statements/code.py
@@ -0,0 +1,5 @@
+# WAP to ask the user for a number. Check if the number is positive or not. If positive, then print "It is a positive number".
+
+#  WAP to ask the user to put a temperature value. If the temperature is greater than 30 then print "Too much hot weather!"
+
+# WAP to take one number from the user and check it is divisible by 5 or not. If divisible by 5 then print "True".
